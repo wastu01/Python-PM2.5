95104347e1c5b0a26d1a490b9a7fb707118179fc
wastu01 <adt107118@gm.ntcu.edu.tw>
2024-09-19 11:14:49 +0800

    移除多餘空白

--- WebAPI.py
+++ WebAPI.py
@@ -1,46 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[19]:
-
-
 import requests
-aqi_url = "https://data.epa.gov.tw/api/v1/aqx_p_432?offset=0&limit=80&api_key=yourapi" 
-# 環保署環境資料開放平臺 試行版 https://data.epa.gov.tw/
-# API 金鑰免費申請 ～
+aqi_url = "https://data.moenv.gov.tw/api/v2/aqx_p_432?offset=0&limit=1000&api_key=your-api-key" 
+
 response = requests.get(aqi_url)
 aqi = response.json()['records']
-# dict >> list
-print(response.status_code)
 
-### 200  正常回傳
-### 404 回傳錯誤
+print(response.status_code)
 print(type(response))
 print(type(aqi))
 
 for wa in aqi :
     print(wa["County"], wa["SiteName"], wa["PM2.5"])
     
-    
 with open("data_pm25.txt", "w", encoding="utf-8") as file:
      for list in aqi:
         file.write(f'{list["County"]} {list["SiteName"]} {list["PM2.5"]}\n')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
